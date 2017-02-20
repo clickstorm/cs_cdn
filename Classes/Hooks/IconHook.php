7091701f5a43af5aa8a58a3d2f8ade21ebbcce7a
Lennart Marschke <marschke@clickstorm.de>
2017-02-20 13:34:27 +0100

    [BUGFIX] #2 fixed the issues with TYPO3 6.2

--- Classes/Hooks/IconHook.php
+++ Classes/Hooks/IconHook.php
@@ -4,6 +4,7 @@ use TYPO3\CMS\Backend\Utility\BackendUtility;
 use TYPO3\CMS\Core\Imaging\IconFactory;
 use TYPO3\CMS\Core\Imaging\Icon;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Filelist\FileList;
 
 /**
  * Created by PhpStorm.
@@ -23,12 +24,23 @@ class IconHook implements \TYPO3\CMS\Filelist\FileListEditIconHookInterface {
     public function manipulateEditIcons(&$cells, &$parentObject)
     {
         /** @var \TYPO3\CMS\Core\Resource\File $file */
-        $iconFactory = GeneralUtility::makeInstance(IconFactory::class);
         $fullIdentifier = $cells['__fileOrFolderObject']->getPublicUrl();
+        $version = \TYPO3\CMS\Core\Utility\VersionNumberUtility::getNumericTypo3Version();
         if ($cells['__fileOrFolderObject'] instanceof \TYPO3\CMS\Core\Resource\File) {
             $url = BackendUtility::getModuleUrl('file_CsCdnCdn', array('target' => $fullIdentifier));
             $renameOnClick = 'top.content.list_frame.location.href = ' . GeneralUtility::quoteJSvalue($url) . '+\'&returnUrl=\'+top.rawurlencode(top.content.list_frame.document.location.pathname+top.content.list_frame.document.location.search);return false;';
-            $cells['cdn'] = '<a href="#" class="btn btn-default" onclick="' . htmlspecialchars($renameOnClick) . '"  title="CDN"> ' . $iconFactory->getIcon('tx-cscdn-replace', Icon::SIZE_SMALL)->render() . '</a>';
+            if($version > 7){
+                $iconFactory = GeneralUtility::makeInstance(IconFactory::class);
+                $cells['cdn'] = '<a href="#" class="btn btn-default" onclick="' . htmlspecialchars($renameOnClick) . '"  title="CDN"> ' . $iconFactory->getIcon('tx-cscdn-replace', Icon::SIZE_SMALL)->render() . '</a>';
+            } else {
+                $cells['cdn'] = '<a href="#" class="btn btn-default" onclick="' . htmlspecialchars($renameOnClick) . '"  title="CDN">
+                    <span class="t3js-icon icon icon-size-small icon-state-default icon-tx-cscdn-replace" data-identifier="tx-cscdn-replace">
+                        <span class="icon-markup">
+                            <img src="/typo3conf/ext/cs_cdn/Resources/Public/Icons/replace.png" width="16" height="16">
+                        </span>
+                    </span>
+                </a>';
+            }
         }
     }
 }
--- ext_localconf.php
+++ ext_localconf.php
@@ -3,7 +3,7 @@
 if (!defined ('TYPO3_MODE'))     die ('Access denied.');
 
 if(TYPO3_MODE=='FE') {
-	$extPath = \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY);
+    $extPath = \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY);
 
     // hook is called after Caching / pages with COA_/USER_INT objects.
     $TYPO3_CONF_VARS['SC_OPTIONS']['tslib/class.tslib_fe.php']['contentPostProc-output'][] = 'Clickstorm\\CsCdn\\Utility\\StringReplacer->contentPostProc_output';
@@ -12,13 +12,13 @@ if(TYPO3_MODE=='FE') {
     $TYPO3_CONF_VARS['SC_OPTIONS']['tslib/class.tslib_fe.php']['contentPostProc-all'][] = 'Clickstorm\\CsCdn\\Utility\\StringReplacer->contentPostProc_output';
 //    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tslib/class.tslib_fe.php']['contentPostProc-output'][] = 'Clickstorm\\CsCdn\\Utility\\StringReplacer->contentPostProc_output';
 
-	// namespace TYPO3\CMS\Frontend\ContentObject\ContentObjectRenderer;
-	$TYPO3_CONF_VARS['FE']['XCLASS']['tslib/class.tslib_content.php'] = 'Clickstorm\\CsCdn\\Utility\\TslibCobj';
+    // namespace TYPO3\CMS\Frontend\ContentObject\ContentObjectRenderer;
+    $TYPO3_CONF_VARS['FE']['XCLASS']['tslib/class.tslib_content.php'] = 'Clickstorm\\CsCdn\\Utility\\TslibCobj';
 
 }
 
 $GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects']['TYPO3\\CMS\\Frontend\\ContentObject\\ContentObjectRenderer'] = array(
-		'className' => 'Clickstorm\\CsCdn\\Utility\\TslibCobj'
+    'className' => 'Clickstorm\\CsCdn\\Utility\\TslibCobj'
 );
 
 $config = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['cs_cdn']);
@@ -32,15 +32,20 @@ if ($config['uploadHook']){
 if ($config['iconHook']){
     $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['fileList']['editIconsHook']['cs_cdn'] = 'EXT:cs_cdn/Classes/Hooks/IconHook.php:Clickstorm\\CsCdn\\Hooks\\IconHook';
 
-    $iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(
-        \TYPO3\CMS\Core\Imaging\IconRegistry::class
-    );
+    $version = \TYPO3\CMS\Core\Utility\VersionNumberUtility::getNumericTypo3Version();
 
-    $iconRegistry->registerIcon(
-        'tx-cscdn-replace', // Icon-Identifier, z.B. tx-myext-action-preview
-        \TYPO3\CMS\Core\Imaging\IconProvider\BitmapIconProvider::class,
-        ['source' => 'EXT:cs_cdn/Resources/Public/Icons/replace.png']
-    );
+    // check which TYPO3 Version is running
+    if($version > 7) {
+        $iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(
+            \TYPO3\CMS\Core\Imaging\IconRegistry::class
+        );
+
+        $iconRegistry->registerIcon(
+            'tx-cscdn-replace', // Icon-Identifier, z.B. tx-myext-action-preview
+            \TYPO3\CMS\Core\Imaging\IconProvider\BitmapIconProvider::class,
+            ['source' => 'EXT:cs_cdn/Resources/Public/Icons/replace.png']
+        );
+    }
 }
 
 
